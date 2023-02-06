97e8a65e1b12a4247d5c8497df0dc1aa196ae4aa
Maria Claudia Rincon Remolina <102395451+mrinconr11@users.noreply.github.com>
2023-02-06 13:42:24 -0500

    Update cnn_5_layers.py

--- cnn_5_layers.py
+++ cnn_5_layers.py
@@ -6,22 +6,43 @@ import pandas as pd
 import numpy as np
 import operator
 import matplotlib.pyplot as plt
-from Dense.functions import mae, rem_scatt, preprocess_test, preprocess, r2
-## Training data
-data = pd.read_csv(r'Creek_Train_largo.csv')
+
+## Import training data
+data = pd.read_csv(r'Creek_Train.csv')
 df = pd.DataFrame(data)
 train_data = np.array(df)
 input_data_train = np.nan_to_num(train_data)
-test = pd.read_csv(r'Creek_Test_largo.csv')
+test = pd.read_csv(r'Creek_Test.csv')
 dft = pd.DataFrame(test)
 test_data = np.array(dft)
 input_data_test = np.nan_to_num(test_data)
 
+## Number of excitation/emission wavelengths in the excitation-emission matrix (EEM)
+## Number of water samples
 n_ex = 160
 n_em = 240
 train_quantity = 144
 test_quantity = 36
 
+## Remove scattering the excitation emission matrix (EEM)
+def rem_scatt(x, nex, nem):
+    ## ranges of the excitation-emission spectra
+    ex = range(240, 602, 2)
+    em = range(250, 802, 2)
+    ind = []
+    bw = 12
+    for i in range(0, len(ex)):
+        for j in range(0, len(em)):
+            # if em[j] <= (ex[i]+bw) and em[j] >= (ex[i]-bw):
+            if em[j] <= (ex[i] + bw):
+                ind.append((i) * nem + (j))
+            # if em[j] <= (ex[i]*2+bw) and em[j] >= (ex[i]*2-bw):
+            if em[j] >= (ex[i] * 2 - bw):
+                ind.append((i) * nem + (j))
+    ## Make scattering 0
+    ind = np.reshape(ind, (1, len(ind)))
+    x[:, ind] = 0.
+    return x
 input_data_train= rem_scatt(input_data_train, 181, 276)
 
 #Reduce matrix size for CNN
@@ -29,6 +50,17 @@ input_data_train = np.reshape(input_data_train, (-1, 181, 276))
 input_data_train = input_data_train[:, 0:80, 0:176]
 input_data_train = np.reshape(input_data_train, (-1, 80*176))
 
+## Normalize EEMs
+def preprocess(x):
+    y = np.copy(x)
+    max_v = np.max(x, axis=0)
+    min_v = np.min(x, axis=0)
+    for i in range(np.size(max_v)):
+        if (max_v[i] - min_v[i] > 0):
+            y[:, i] = (x[:, i] - min_v[i]) / (max_v[i] - min_v[i])
+        else:
+            continue
+    return y, max_v, min_v  # max and min vectors are needed to renormalize
 input_data_train, max_v, min_v = preprocess(input_data_train)
 
 # Input style for CNN, requires 4D tensor
@@ -44,13 +76,14 @@ input_data_test = preprocess_test(input_data_test, max_v, min_v)
 
 input_data_test_CNN = np.reshape(input_data_test, (-1, 80, 176, 1))
 
-atrain_concentration = pd.read_csv(r'../CNN/Creek_Train_Concentrations_largo.csv')
+atrain_concentration = pd.read_csv(r'../CNN/Creek_Train_Concentrations.csv')
 dfc = pd.DataFrame(train_concentration)
 train_concentration = np.array(dfc)
-test_concentration = pd.read_csv(r'../CNN/Creek_Test_Concentrations_largo.csv')
+test_concentration = pd.read_csv(r'../CNN/Creek_Test_Concentrations.csv')
 dfct = pd.DataFrame(test_concentration)
 test_concentration = np.array(dfct)
 
+## Separate concentration for naphthenic acids (NAs) and phenol for training and testing
 train_Naph = train_concentration[:, 0]
 train_Phenol = train_concentration[:, 1]
 train_Naph.shape = [train_quantity, 1]
@@ -59,125 +92,79 @@ test_Naph = test_concentration[:, 0]
 test_Phenol = test_concentration[:, 1]
 test_Naph.shape = [test_quantity, 1]
 test_Phenol.shape = [test_quantity, 1]
-##
-mae_naph_test = []
-r2_naph_test = []
-mae_phenol_test = []
-r2_phenol_test = []
-naph_predicted_test = []
-phenol_predicted_test = []
 
-for i in range(1):
-    def naph(input):
-        layer1 = Conv2D(32, (7, 7), padding='same')(input)
-        layer1 = BatchNormalization()(layer1)
-        layer1 = Activation('elu')(layer1)
-        layer1 = MaxPooling2D((2,2))(layer1)
-        layer2 = Conv2D(32, (7, 7), padding='same')(layer1)
-        layer2 = BatchNormalization()(layer2)
-        layer2 = Activation('elu')(layer2)
-        layer3 = Conv2D(32, (7, 7), padding='same')(layer2)
-        layer3 = BatchNormalization()(layer3)
-        layer3 = Activation('elu')(layer3)
-        layer4 = Conv2D(32, (7, 7), padding='same')(layer3)
-        layer4 = BatchNormalization()(layer4)
-        layer4 = Activation('elu')(layer4)
-        layer5 = Conv2D(32, (7, 7), padding='same')(layer4)
-        layer5 = BatchNormalization()(layer5)
-        layer5 = Activation('elu')(layer5)
-        layer5 = MaxPooling2D((2, 2))(layer5)
-        flatten = Flatten()(layer5)
-        dense = Dense(units=10, activation='elu')(flatten)
-        output = Dense(units=1, activation='elu')(dense)
-        return output
+## CNN for NAs, 5 convolutional layers. Predicts NAs concentrations
+def naph(input):
+    layer1 = Conv2D(32, (7, 7), padding='same')(input)
+    layer1 = BatchNormalization()(layer1)
+    layer1 = Activation('elu')(layer1)
+    layer1 = MaxPooling2D((2,2))(layer1)
+    layer2 = Conv2D(32, (7, 7), padding='same')(layer1)
+    layer2 = BatchNormalization()(layer2)
+    layer2 = Activation('elu')(layer2)
+    layer3 = Conv2D(32, (7, 7), padding='same')(layer2)
+    layer3 = BatchNormalization()(layer3)
+    layer3 = Activation('elu')(layer3)
+    layer4 = Conv2D(32, (7, 7), padding='same')(layer3)
+    layer4 = BatchNormalization()(layer4)
+    layer4 = Activation('elu')(layer4)
+    layer5 = Conv2D(32, (7, 7), padding='same')(layer4)
+    layer5 = BatchNormalization()(layer5)
+    layer5 = Activation('elu')(layer5)
+    layer5 = MaxPooling2D((2, 2))(layer5)
+    flatten = Flatten()(layer5)
+    dense = Dense(units=10, activation='elu')(flatten)
+    output = Dense(units=1, activation='elu')(dense)
+    return output
 
-    inputs = Input(shape=(80, 176, 1))
-    naph_output = naph(inputs)
-    naph_model = Model(inputs=inputs, outputs=naph_output)
-    naph_model.compile(optimizer='adam', loss='mean_squared_error')
-    deep_auto_history = naph_model.fit(
-        x=input_data_train_CNN,
-        y=train_Naph,
-        epochs=500,
-        batch_size=32,
-        shuffle=True,
-        validation_split=0.2
-    )
-    naph_predicted = naph_model.predict(input_data_test_CNN)
-    mae_naph = float("{:.3f}".format(mae(test_Naph, naph_predicted)))
-    r2_naph = float("{:.3f}".format(r2(test_Naph, naph_predicted)))
-    mae_naph_test.append(mae_naph)
-    r2_naph_test.append(r2_naph)
-    naph_predicted_test.append(naph_predicted)
+## The input is the EEM
+inputs = Input(shape=(80, 176, 1))
+naph_output = naph(inputs)
+naph_model = Model(inputs=inputs, outputs=naph_output)
+naph_model.compile(optimizer='adam', loss='mean_squared_error')
 
-    def phenol(input):
-        layer1 = Conv2D(32, (7, 7), padding='same')(input)
-        layer1 = BatchNormalization()(layer1)
-        layer1 = Activation('elu')(layer1)
-        layer1 = MaxPooling2D((2,2))(layer1)
-        layer2 = Conv2D(32, (7, 7), padding='same')(layer1)
-        layer2 = BatchNormalization()(layer2)
-        layer2 = Activation('elu')(layer2)
-        layer3 = Conv2D(32, (7, 7), padding='same')(layer2)
-        layer3 = BatchNormalization()(layer3)
-        layer3 = Activation('elu')(layer3)
-        layer4 = Conv2D(32, (7, 7), padding='same')(layer3)
-        layer4 = BatchNormalization()(layer4)
-        layer4 = Activation('elu')(layer4)
-        layer5 = Conv2D(32, (7, 7), padding='same')(layer4)
-        layer5 = BatchNormalization()(layer5)
-        layer5 = Activation('elu')(layer5)
-        layer5 = MaxPooling2D((2, 2))(layer5)
-        flatten = Flatten()(layer5)
-        dense = Dense(units=10, activation='elu')(flatten)
-        output = Dense(units=1, activation='elu')(dense)
-        return output
+## Train the CNN
+deep_auto_history = naph_model.fit(
+    x=input_data_train_CNN,
+    y=train_Naph,
+    epochs=500,
+    batch_size=32,
+    shuffle=True,
+    validation_split=0.2
+)
 
-    inputs = Input(shape=(80, 176, 1))
-    phenol_output = phenol(inputs)
-    phenol_model = Model(inputs=inputs, outputs=phenol_output)
-    phenol_model.compile(optimizer='adam', loss='mean_squared_error')
-    deep_auto_history = phenol_model.fit(
-        x=input_data_train_CNN,
-        y=train_Phenol,
-        epochs=500,
-        batch_size=32,
-        shuffle=True,
-        validation_split=0.2
-    )
-    phenol_predicted = phenol_model.predict(input_data_test_CNN)
-    mae_phenol = float("{:.3f}".format(mae(test_Phenol, phenol_predicted)))
-    r2_phenol = float("{:.3f}".format(r2(test_Phenol, phenol_predicted)))
-    mae_phenol_test.append(mae_phenol)
-    r2_phenol_test.append(r2_phenol)
-    phenol_predicted_test.append(phenol_predicted)
-    phenol_model.save('phenol_model_creek.h5')
-##
-r2_index_naph, r2_value_naph = max(enumerate(r2_naph_test), key=operator.itemgetter(1))
-mae_index_naph, mae_value_naph = min(enumerate(mae_naph_test), key=operator.itemgetter(1))
-r2_index_phenol, r2_value_phenol = max(enumerate(r2_phenol_test), key=operator.itemgetter(1))
-mae_index_phenol, mae_value_phenol = min(enumerate(mae_phenol_test), key=operator.itemgetter(1))
+## Make predictions of NAs concentration with the test data set
+naph_predicted = naph_model.predict(input_data_test_CNN)
 
-labels = ['Naphthenic','Phenol']
-concentration_predicted = [naph_predicted_test[mae_index_naph], phenol_predicted_test[mae_index_phenol]]
-concentration_predicted = np.array(concentration_predicted)
-concentration_predicted.shape = [2, test_quantity]
-concentration_predicted = concentration_predicted.transpose()
+## The evaluation metrics are the mean absolute error (MAE) and R2
+def mae(real, predicted):
+    mae = np.sum(abs(real-predicted), axis=0)
+    mae= mae/len(real)
+    mae = np.nanmean(mae)
+    return mae
 
-real_datafrane = pd.DataFrame(test_concentration, columns=labels)
-predicted_dataframe = pd.DataFrame(concentration_predicted, columns=labels)
+def r2(real, predicted):
+    mean = np.mean(real, axis=0)
+    first_errors_autoencoder = np.sum((real-predicted)**2, axis=0)
+    second_errors_autoencoder = np.sum((real-mean)**2, axis=0)
+    r2_autoencoder = 1-first_errors_autoencoder/second_errors_autoencoder
+    r2_autoencoder[r2_autoencoder == -inf] = nan
+    r2 = np.nanmean(r2_autoencoder)
+    return r2
+mae_naph = float("{:.3f}".format(mae(test_Naph, naph_predicted)))
+r2_naph = float("{:.3f}".format(r2(test_Naph, naph_predicted)))
 
+## Plot predicted concentrations vs real concentrations
 plt.figure(figsize=(10,10))
-plt.title('5 CNN layers', fontsize=20)
-plt.scatter(real_datafrane['Naphthenic'], predicted_dataframe['Naphthenic'], c='crimson', label="Naphthenic")
-plt.scatter(real_datafrane['Phenol'], predicted_dataframe['Phenol'], c='blue', label='Phenol')
+plt.title('NAs concentrations', fontsize=20)
+plt.scatter(test_Naph, naph_predicted, c='crimson', label="Naphthenic")
 
 x = np.linspace(0, 100)
 plt.plot(x, x, c='black', linestyle='dotted')
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
-plt.xlabel('Measured NAs (mg/L) and Phenol (${\mu}$g/L)', fontsize=20)
-plt.ylabel('Predicted NAs (mg/L) and Phenol (${\mu}$g/L)', fontsize=20)
+plt.xlabel('Measured NAs (mg/L)', fontsize=20)
+plt.ylabel('Predicted NAs (mg/L)', fontsize=20)
 plt.axis('square')
 plt.grid(linestyle='dotted', linewidth=1)
 plt.legend(fontsize=20)
